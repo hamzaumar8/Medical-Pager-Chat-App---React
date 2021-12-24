b64915308a69961302610b48e43815d27f65f6b2
Hamza Umar <humar6078@gmail.com>
2021-12-25 00:51:57 +0800

    channel lsit header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import { ChannelContainer ,ChannelListContainer } from './components/index';
+import './App.css';
+
+const apiKey = "paadqtuv2tvx";
+const client =  StreamChat.getInstance(apiKey);
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Medikal Pager</h1>
+        <div className='app__wrapper'>
+            <Chat client={client} theme="team light">
+                <ChannelListContainer />
+                <ChannelContainer />
+            </Chat>
         </div>
     )
 }
 
-export default App
+export default App;
