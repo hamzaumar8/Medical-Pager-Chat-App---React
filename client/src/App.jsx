bc3731689aa4d403795c54d4b6bd09977c476564
Hamza Umar <humar6078@gmail.com>
2021-12-25 05:31:06 +0800

    Auth Page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
-import { ChannelContainer ,ChannelListContainer } from './components/index';
+import { ChannelContainer ,ChannelListContainer, Auth} from './components/index';
 import './App.css';
+
 
 const apiKey = "paadqtuv2tvx";
 const client =  StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 
 const App = () => {
+    if(!authToken) return <Auth />
     return (
         <div className='app__wrapper'>
             <Chat client={client} theme="team light">
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react';
 
-
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
 import LogoutIcon from '../assets/logout.png';
 
@@ -32,6 +33,27 @@ const ChannelListContainer = () => {
            <SideBar /> 
            <div className='channel-list__list__wrapper'>
                <CompanyHeader />
+               <ChannelSearch />
+               <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList  {...listProps} type="team" />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview {...previewProps} type="team" />
+                    )}
+                />
+               <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList  {...listProps} type="messaging" />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview {...previewProps} type="messaging" />
+                    )}
+                />
            </div>
         </>
     )
