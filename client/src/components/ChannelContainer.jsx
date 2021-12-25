11773af51efae3c3b723de1bd2c059edea61582a
Hamza Umar <humar6078@gmail.com>
2021-12-25 20:33:02 +0800

    channel message done

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
-import { Channel, userChatContext } from 'stream-chat-react'
+import { Channel, useChatContext, MessageTeam } from 'stream-chat-react'
 
-import {ChannelInner, CreateChannel, EditChannel, TeamMessage} from './index'
+import {ChannelInner, CreateChannel, EditChannel} from './index'
 
-const ChannelContainer = () => {
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+
+    const { channel } = useChatContext();
+
+    if(isCreating) {
+        return (
+            <div className='channel__container'>
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+            </div>
+        );
+    }
+    
+    if(isEditing) {
+        return (
+            <div className='channel__container'>
+                <EditChannel setIsEditing={setIsEditing} />
+            </div>
+        );
+    }
+
+    const EmptyState = () => (
+        <div className='channel-empty__container'>
+            <p className='channel-empty__first'>This is the begining of your chat history.</p>
+            <p className='channel-empty__second'>Send message, attachments, links, emojis and more!</p>
+        </div>
+    )
+
     return (
-        <div>
+        <div className='channel__container'>
+            <Channel 
+                EmptyStateIndicator={EmptyState} 
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+            >
+                <ChannelInner setIsEditing={setIsEditing}/>
+            </Channel>
             
         </div>
     )
