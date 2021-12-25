6b19c0709784662516fbfab21e43d556c43ac3cd
Hamza Umar <humar6078@gmail.com>
2021-12-25 19:45:58 +0800

    serve auth done - connect with client auth

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
 
+const cookies = new Cookies();
+
+
 const initialState = {
     fullName: '',
     username: '',
@@ -21,9 +24,27 @@ const Auth = () => {
         setForm({ ...form, [event.target.name]: event.target.value });
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(form)
+        
+        const { fullName, username, password, phoneNumber, avatarURL} = form;
+
+        const URL = 'http://localhost:500/auth';
+
+        const { data: { token, userId, hashedPassword} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`,{ fullName, username, password, phoneNumber, avatarURL,});
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     const switchMode = () => {
