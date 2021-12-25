73eec1dac3f9bb8a57c289e43f931ef047a86298
Hamza Umar <humar6078@gmail.com>
2021-12-25 20:09:38 +0800

    Channel Containein components init

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Channel, userChatContext } from 'stream-chat-react'
+
+import {ChannelInner, CreateChannel, EditChannel, TeamMessage} from './index'
 
 const ChannelContainer = () => {
     return (
         <div>
-            ChannelContainer
+            
         </div>
     )
 }
